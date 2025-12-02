425ed91a4f254ef6f6fe5e04703c4d271a431e4f
injal123 <162284560+injal123@users.noreply.github.com>
2025-12-02 10:26:51 +0545

    initial commit

---
+++ backend/src/routes/message.route.js
@@ -0,0 +1,20 @@
+import express from 'express';
+
+const router = express.Router();
+
+
+
+
+
+router.get('/send', (req, res) => {
+    res.send("Send message Endpoint");
+})
+
+
+
+
+
+
+
+
+export default router;
