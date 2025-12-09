9f1fada82eeac84ddf5094f125d19e4eb0af3597
injal123 <162284560+injal123@users.noreply.github.com>
2025-12-09 16:53:09 +0545

    message-routes added

---
+++ backend/src/controllers/message.controller.js
@@ -0,0 +1,133 @@
+// $in $or $ne
+
+
+import User from "../models/User.js";
+import Message from "../models/Message.js";
+import cloudinary from '../lib/cloudinary.js';
+
+
+export const getAllContacts = async (req, res) => {
+    try {
+        const loggedInUserId =  req.user._id;  // next, get all users except this loggedInUserId.
+        const filteredUsers = await User.find({ _id: { $ne: loggedInUserId } }).select("-password");
+
+        res.status(200).json(filteredUsers);
+
+    } catch (error) {
+        console.error("Error in getAllContacts:", error);
+        res.status(500).json({ message: "Internal server error" });
+    }
+};
+
+
+
+export const getMessagesByUserId = async (req, res) => {
+  try {
+    const myId = req.user._id;                 // you
+    const { id: chatPartnerId } = req.params;       // the user you are chatting with
+    
+    if (!chatPartnerId) {
+      return res.status(400).json({ error: "Chat partner ID is required." });
+    }
+
+    const messages = await Message.find({
+      $or: [
+        { senderId: myId, receiverId: chatPartnerId },
+        { senderId: chatPartnerId, receiverId: myId }
+      ]
+    })
+      .sort({ createdAt: 1 });   //sort messages by ascending order of time- older messages then newer.
+
+    return res.status(200).json(messages);
+
+  } catch (error) {
+    console.error("Error in getMessagesByUserId:", error);
+    return res.status(500).json({ error: "Internal server error" });
+  }
+};
+
+
+
+export const sendMessage = async (req, res) => {
+    try {
+        const { text, image } = req.body;
+
+        const senderId = req.user._id;
+        const { id: receiverId } = req.params;
+
+        if(!text && !image) {
+            return res.status(400).json({ message: "Message text or image is required" });
+        }
+
+        let imageUrl;
+        if (image) {
+            const uploadResponse = await cloudinary.uploader.upload(image,
+                { folder: 'Chatterly/imageMessage' }
+            );
+            imageUrl = uploadResponse.secure_url;
+        }
+
+        const newMessage = new Message({
+            senderId,
+            receiverId,
+            text,
+            image: imageUrl
+        });
+
+        await newMessage.save();
+
+        // socket.io 
+
+        res.status(200).send(newMessage);
+
+    } catch (error) {
+        console.error("Error in sendMessage controller:", error);
+        res.status(500).json({ message: "Internal server error" });
+    }
+};
+
+
+
+
+export const getAllChattedPartners = async (req, res) => {
+    try {
+        const loggedInUserId = req.user._id;
+
+        // find all the messages where the logged-in user is either sender or receiver.
+        const messages = await Message.find({
+            $or: [
+                { senderId: loggedInUserId },
+                { receiverId: loggedInUserId }
+            ]
+        });
+
+        // res.status(200).send(messages); // toFutureMe: Uncomment this line to see messages value.
+
+        const partnersId = [];
+
+        // Pick the other person's ID from each message where its not loggedInUserId.
+        messages.forEach( msg => {    // Since, ObjectId !== ObjectId → use toString() for comparision.
+            if (msg.senderId.toString() === loggedInUserId.toString()) {
+                partnersId.push(msg.receiverId.toString());
+            }
+            else {
+                partnersId.push(msg.senderId.toString());
+            }
+        });
+
+        // Remove duplicates: e.g., [user1, user2, user1] --> [user1, user2]
+        const uniquePartnerIds = [...new Set(partnersId)];
+
+        // Fetch user details of chat partners.
+        const chatPartners = await User.find({
+            _id: { $in: uniquePartnerIds }
+        }).select("-password");
+
+        res.status(200).send(chatPartners);
+
+
+    } catch (error) {
+        console.error("Error in getAllChattedPartners:", error);
+        res.status(500).json({ message: "Internal server error" });
+    }
+};
--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,15 +1,19 @@
 import express from 'express';
+import { getAllContacts, getMessagesByUserId, sendMessage, getAllChattedPartners } from '../controllers/message.controller.js';
+
+import { arcjetProtection } from '../middleware/arcjet.middleware.js';
+import { protectRoute } from '../middleware/auth.middleware.js';
 
 const router = express.Router();
 
 
+//  arcjetProtection run 1st -> protectRoute -> other controller endpoints below.
+router.use(arcjetProtection, protectRoute );   // protectRoute has req.user heheh.
 
-
-
-router.get('/send', (req, res) => {
-    res.send("Send message Endpoint");
-})
-
+router.get('/contacts', getAllContacts );  // get all users except logged in user.
+router.get('/chats', getAllChattedPartners);  // get all users with whom loggedIn user has chatted.
+router.get('/:id', getMessagesByUserId);  // get all messages between logged in user and user with :id
+router.post('/send/:id', sendMessage );   // send message to user with :id
 
 
 
